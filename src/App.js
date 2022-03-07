d92862894701786777fa1f6e4675dea20c229f14
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2022-03-07 21:00:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+
   console.log(`렌더링시 항상 실행됨`);
 
   useEffect(() => {
@@ -17,13 +18,12 @@ function App() {
       console.log(`키워드 6글자이상부터 실행됨`, keyword);
     }
   }, [keyword]);
+
   useEffect(() => {
     if (counter !== 0) {
       console.log(`'counter'가 1이상이면 실행됨`);
     }
   }, [counter]);
-
-  //keyword가 빈칸이 아니고 길이가 5이상일때 console.log를 실행한다
 
   return (
     <div>
